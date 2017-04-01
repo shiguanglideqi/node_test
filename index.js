8dd393cfed5d50d3266c82900899a78e164ac16d
shiguanglideqi <125435696@qq.com>
2017-04-01 09:38:56 +0800

    first pull

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-// require('dotenv').config();
-// const express = require('express');
-
-// const app = express();
-// const port = process.env.PORT || 3000;
-
-// app.get('/', (req, res) => {
-//     res.send('\n\nHello, world!\n\n');
-// });
-
-// app.listen(port, () => {
-//     console.log(`listening on port ${ port }`);
-// });
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');// 解析body字段模块
